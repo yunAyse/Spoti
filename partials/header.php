cc40e8d78e8397e9579c18de7fd4a07be6e761e0
yunAyse <aysepc0@gmail.com>
2024-02-13 13:46:53 +0100

    update header

--- partials/header.php
+++ partials/header.php
@@ -24,7 +24,7 @@ require_once('../utils/database.php');
       <div class="container ">
         <div class="row">
           <div class="log-out d-flex mb-3">
-            <a href="/php-pdo/CLONE-Spotify/index.php" style="text-decoration: none; color: #fff;">
+            <a href="../index.php" style="text-decoration: none; color: #fff;">
               <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-box-arrow-left text-light me-3" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
                 <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
@@ -35,7 +35,7 @@ require_once('../utils/database.php');
           <nav class="navbar rounded-3" style="background-color: #292929">
             <div class="container-md g-col-4 d-flex flex-column align-items-center">
 
-              <a href="/php-pdo/CLONE-Spotify/pages/homepage.php" class="mb-2">
+              <a href="../pages/homepage.php" class="mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house-fill text-light" viewBox="0 0 16 16">
                   <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
                   <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
@@ -54,7 +54,7 @@ require_once('../utils/database.php');
       </div>
         <div class="container-md g-col-4 rounded-3 py-4 mt-2" style="background-color: #292929; height: 600px;">
           <div class="d-flex flex-column align-items-center">
-            <a href="/php-pdo/CLONE-Spotify/pages/playlist.php">
+            <a href="../pages/playlist.php">
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-collection-fill text-light" viewBox="0 0 16 16">
               <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1" />
             </svg>
